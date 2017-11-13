e14d697165e6cb6478a25da82af252cb02effa1a
Tim Walsh <timothyryanwalsh@gmail.com>
2017-11-12 20:48:45 -0500

    Bump version to 1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'addext',
-    version = '1.0.0',
+    version = '1.0.1',
     url = 'https://github.com/timothyryanwalsh/addext',
     author = 'Tim Walsh',
     author_email = 'timothyryanwalsh@gmail.com',
