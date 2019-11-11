a12939a564c47e9f63b916a49603ba60c7cee307
Tim Walsh <timothyryanwalsh@gmail.com>
2019-11-10 19:57:04 -0800

    Bump version to 1.1.0

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 from setuptools import setup
 
 setup(
-    name = 'addext',
-    version = '1.0.1',
-    url = 'https://github.com/timothyryanwalsh/addext',
-    author = 'Tim Walsh',
-    author_email = 'timothyryanwalsh@gmail.com',
+    name='addext',
+    version='1.1.0',
+    url='https://github.com/timothyryanwalsh/addext',
+    author='Tim Walsh',
+    author_email='timothyryanwalsh@gmail.com',
     packages=['addext'],
     package_data={
         'addext': ['pronom.db']
     },
     include_package_data=True,
-    py_modules = ['addext'],
-    scripts = ['addext/addext.py'],
-    install_requires = ['inquirer'],
-    description = 'Adds file extensions to files based on their PRONOM identifiers (PUIDs).',
-    keywords = 'extensions identification',
-    platforms = ['POSIX', 'Windows'],
-    classifiers = [
+    py_modules=['addext'],
+    scripts=['addext/addext.py'],
+    install_requires=['inquirer'],
+    description='Adds file extensions to files based on their PRONOM identifiers (PUIDs).',
+    keywords='extensions identification',
+    platforms=['POSIX', 'Windows'],
+    classifiers=[
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
         'Intended Audience :: End Users/Desktop',
